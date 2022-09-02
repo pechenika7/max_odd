97ec0f8221c275aad72cc39da884574553f02dc7
pechenika7 <112207398+pechenika7@users.noreply.github.com>
2022-09-02 13:20:06 +0300

    msx_odd

---
+++ max odd.pas
@@ -0,0 +1,29 @@
+﻿program vika;
+var f:text;
+    temp:integer;
+    prom,res:integer;
+    
+procedure p;
+begin
+  if (odd(temp)) then prom+=1 
+    else
+    begin
+      if (prom>res) then res:=prom;
+      prom:=0;
+    end;
+end;
+
+begin
+  assign (f,'d:\123\random_numbs3.txt');
+  reset(f);
+  prom:=0;
+  res:=0;
+  while not(eof(f)) do
+  begin
+    readln(f,temp);
+    p;
+  end;
+  if (prom>res) then res:=prom;
+  close(f);
+  writeln (res);
+end.
---
+++ max odd2.pas
@@ -0,0 +1,30 @@
+﻿program vika;
+const str=7;
+var f:text;
+    temp,s5,s5max:integer;
+    prom:integer;
+
+begin
+  assign (f,'d:\123\random_numbs3.txt');
+  reset(f);
+  prom:=0;
+  s5:=0;
+  while not(eof(f)) do
+  begin
+    readln(f,temp);
+    if (odd(temp)) then
+    begin
+      prom+=1;
+      s5+=temp;
+    end
+    else
+    begin
+      if (prom=str) and (s5>s5max) then s5max:=s5;
+      prom:=0;
+      s5:=0;
+    end;
+  end;
+  if (prom=str) and (s5>s5max) then s5max:=s5;
+  close(f);
+  writeln (s5max);
+end.
